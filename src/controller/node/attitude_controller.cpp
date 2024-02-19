311e6443d98522575e1b6e5244aedf836bbbfc38
CYMElee <leesparks8@gmail.com>
2024-02-20 00:02:36 +0800

    add_optitrack_sensing

--- src/controller/node/attitude_controller.cpp
+++ src/controller/node/attitude_controller.cpp
@@ -1,19 +1,55 @@
 #include "ros/ros.h"
-#include "geometry_msgs/Point.h"
+
+#include "geometry_msgs/TwistStamped" //attitude velocity msgs
+#include "geometry_msgs/PoseStamped" //attitude msgs
+
 #include "Eigen/Dense"
+
+
+using namespace Eigen ;
+//attitude control gains
+Matrix<float, 3, 3> KR;
+Matrix<float, 3, 3> Kw;
+Matrix<float, 3, 3> Ki;
+
+Matrix<float, 3, 3> E;
+Matrix<float, 3, 3> eR;
+Matrix<float, 3, 3> eW;
+Matrix<float, 3, 3> eRiout;
+
+KR << 17.0, 0 , 0 ,
+      0  , 15.0,0
+      0  , 0,  8.0;
+
+Kw << 6.0, 0 , 0 ,
+      0  , 5.5,0
+      0  , 0,  3.0;
+
+Ki << 5.5, 0 , 0 ,
+      0  , 4.5,0
+      0  , 0,  3;
+
+
+// 
 
 
 int main(int argc,char **argv)
 {
     ros::init(argc,argv,"attitude_controller");
     ros::NodeHandle nh;
-
+    ros::Subscriber desire_agvr = nh.subscribe<std_msgs::Float32MultiArray>
+        ("gripper/desire_agvr",10,desire_agvr_cb);
+    ros::Subscriber desire_agvr = nh.subscribe<std_msgs::Float32MultiArray>
+        ("gripper/measure_attitude",10,measure_attitude_cb);
+    ros::Subscriber desire_agvr = nh.subscribe<std_msgs::Float32MultiArray>
+        ("gripper/measure_attitude",10,measure_attitude_cb);
+    ros::Subscriber desire_agvr = nh.subscribe<std_msgs::Float32MultiArray>
+        ("gripper/measure_attitude",10,measure_attitude_cb);                                                
 
     ros::Rate rate(100);
 
     while(ros::ok)
     {
-
 
 
 
---
+++ src/sensor/node/platform.cpp
@@ -0,0 +1,19 @@
+#include "ros/ros.h"
+#include "geometry_msgs/PointStamped.h"
+
+
+
+int main(int argc,char **argv)
+{
+    ros::init(argc,argv,"platform_position");
+    ros NodeHandle nh;
+
+
+    
+
+
+
+
+
+    return 0;
+}
