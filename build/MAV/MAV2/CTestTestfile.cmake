203afeb178151c98bfbd70db9a2b96624c81f727
LEE-YA-JIE <leesparks8@gmail.com>
2024-08-27 14:41:43 +0800

    fix_mav1_eigen_index

--- build/MAV/MAV2/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/lee/workspace/ROS_ws/ex_ws/src/MAV/MAV2
-# Build directory: /home/lee/workspace/ROS_ws/ex_ws/build/MAV/MAV2
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
